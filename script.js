5fc2c3da476c4bd34b599db98894d6de9a4e77da
brayan gonzalez <brayangonzalez1417@gmail.com>
2026-03-25 20:12:13 -0500

    practica javascript

---
+++ script.js
@@ -0,0 +1,58 @@
+// console.log("hola")
+
+
+// 1_PRACTICA
+
+// let edad = Number(prompt ("dime tu nombre"))
+// console.log(edad)
+
+// let nombre = prompt ("cual es tu nombre");
+
+// let edad = Number (prompt ("cual es tu edad"));
+
+// let correo = prompt ( "cual es tu correo");
+
+// console.log(nombre,edad,correo);
+
+
+
+// 2_PRACTICA
+
+// let nombre = prompt ("cual es tu nombre")
+
+// let precio =  prompt ("el precio es de :") 
+
+// let descuento = Number(prompt ("esgoje tu descuento"))
+
+// let precioDe=(precio*descuento)/100
+// alert ( "tu precio con descuento es del" + precioDe)
+
+// let precioF=(precio-precioDe)
+// alert("tu precio final es de" + precioF)
+
+
+// let m = Number (prompt("dame tu numero en metros"))
+
+
+
+// metros a kilometros
+
+// let m = Number (prompt("metros"))
+// let km = (m/1000)
+// alert (km+"kilometros")
+
+
+// kilometros a metros
+
+// let km2 = Number(prompt("kilometros"))
+// let m2 = (km2*1000)
+// alert (m2+" metros")
+
+
+
+
+
+
+
+
+
