16e93d1edf1c1dc9fe57a642e830120b70753ac7
brayan gonzalez <brayangonzalez1417@gmail.com>
2026-03-26 21:50:55 -0500

    ejercicos en clase

--- script.js
+++ script.js
@@ -115,7 +115,59 @@
 
 // convertidor de milimetros a centimetros
 
-let milimetros = Number(prompt("ingresa los milimetros"))
-let centimetros = (milimetros/10)
-alert(centimetros + "cm")
+// let milimetros = Number(prompt("ingresa los milimetros"))
+// let centimetros = (milimetros/10)
+// alert(centimetros + "cm")
+
+
+
+
+// let nombre = 'juan'
+// let edad = '33'
+// let correo = 'juan@gmail.com'
+
+// console.log ('hola, tu nombre es: ' + nombre + 'tienes' + edad + 'años y tu correo es:' + correo)
+
+
+
+// let juan = (prompt("tiene que pagar el doble porque fue el que mas comio"))
+// let personas = (prompt(""))
+
+// let juan = (prompt("cuanto  fue costo la cena"))
+
+// cual fue el que mas comio
+
+// numero de peronas
+
+
+
+// ejercicio en clase
+
+// let peronas = Number(prompt("numero de perosnas"));
+// let comida = (prompt("¡quien fue el que mas comio?"));
+// let valor = Number(prompt(" el valor de la cena fue de"));
+
+// let pagoNormal = valor/(peronas + 1);
+// let pagoDoble = pagoNormal * 2
+
+// console.log ("numero de personas:" + peronas );
+// console.log ("valor total:" + valor)
+// console.log ("los que pagan normal ponen:" + pagoNormal)
+// console.log ("el que comio mas:" + comida +  "paga el doble" + pagoDoble)
+
+
+
+// usuario un monto , cauntos meses, despues de 6 meses vas a gnar de inteeres al 10 %
+
+let monto = Number (prompt("cuanta plata va meter"))
+let meses = Number (prompt("cuantos meses"))
+let interesAldies = 0.10
+
+let totalInteres = monto * interesAlDies + meses;
+
+let montoTotal = monto + totalInteres
+
+alert("En" + meses + "meses ganaras" + totalInteres + "por interes")
+alert("tu saldo total sera de" + montoTotal)
+
 
